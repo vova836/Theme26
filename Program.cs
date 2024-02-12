d375a31ad156e9ba79466fa5c7a54f0580d86cc4
vova836 <123488703+vova836@users.noreply.github.com>
2024-02-12 15:57:08 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace Theme26;
+
+public static class Program
+{
+    static void Main()
+    {
+        // 1. Ввести две даты и определить количество дней между ними.
+        DateTime date1 = new DateTime(2022, 2, 15);
+        DateTime date2 = new DateTime(2022, 3, 20);
+        int daysDifference = DateHelper.GetDaysDifference(date1, date2);
+        Console.WriteLine($"Days between the two dates: {daysDifference}");
+
+        Console.WriteLine();
+
+        // 2. Определить, сколько дней осталось до конца месяца.
+        DateTime dateNow = DateTime.Today;
+        int daysRemainingInMonth = DateHelper.GetDaysRemainingInMonth(dateNow);
+        Console.WriteLine($"Days remaining in the month: {daysRemainingInMonth}");
+
+        Console.WriteLine();
+
+        // 3. Ввести свою дату рождения и вывести информацию
+        DateTime birthday = new DateTime(2000, 5, 25);
+        DateHelper.GetBirthdayInfo(birthday);
+    }
+}
+	}
+}
